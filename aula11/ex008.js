882c145c0104d4e11da066a7f0d73c30478ad097
Alexandre Junior <alexandrejunior408@gmail.com>
2022-03-04 13:51:57 -0300

    adiciona exercicio de node.js utilizando condições

---
+++ aula11/ex008.js
@@ -0,0 +1,15 @@
+/*COMANDOS SEQUÊNCIAIS
+
+var velocidade = 60.5;
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`);
+console.log(`Dirija sempre usando cinto de segurança!`);
+
+*/
+
+//CONDIÇÃO
+var velocidade = 12;
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`);
+if (velocidade > 60) {
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
