7d4f9da04800c40abc70c0a3dabe093df3763f1b
Alexandre Junior <alexandrejunior408@gmail.com>
2022-03-06 14:17:31 -0300

    adiciona ambiente de estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+let c = 1;
+//faça
+do {
+  console.log(`passo ${c}`);
+  c++;
+} while (
+  //enquanto
+  c <= 6
+);
+
+/* let c = 1;
+
+//enquanto
+while (c < 6) {
+  console.log(`passo ${c}`);
+  c++;
+}
+ */
